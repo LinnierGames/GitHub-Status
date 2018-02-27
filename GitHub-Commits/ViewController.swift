8fb920a532ef6eea025d981a0db4417fd7927c81
LinnierGames <esericksanc@gmail.com>
2018-02-26 20:17:35 -0800

    Initial Commit

---
+++ GitHub-Commits/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHub-Commits
+//
+//  Created by Erick Sanchez on 2/26/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
